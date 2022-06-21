59a94579b6e25aebc273cd89f160784b6d188028
Marcelo Mascarenhas <70808580+marcelo-mascarenhas@users.noreply.github.com>
2022-06-21 18:26:43 -0300

    Commit 6

--- Customer.java
+++ Customer.java
@@ -30,9 +30,6 @@ public class Customer {
          Rental each = (Rental) rentals.nextElement();
 
          frequentRenterPoints += each.getFrequentRenterPoints();
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
