0e2fd544d679f2be27496b2df257b3b61e4641f8
Marcelo Mascarenhas <70808580+marcelo-mascarenhas@users.noreply.github.com>
2022-06-22 23:10:50 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -4,22 +4,8 @@ public abstract class Price {
    
    public abstract double getCharge(int daysRented);
    
-   public int getFrequentRenterPoints(int daysRented){
-         
-         int frequentRenterPoints = 0;
-         
-         frequentRenterPoints ++;
-
-         if ((this.getPriceCode() == Movie.NEW_RELEASE) &&  
-            daysRented > 1){ 
-            frequentRenterPoints ++;
-         }
-
-      return frequentRenterPoints;   
-    }
-
-
-
-
+   public int getFrequentRenterPoints(int daysRented) {
+       return 1;
+   }
 
 }
